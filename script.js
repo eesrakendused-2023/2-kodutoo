809ebbf514b104ef0f11785c8aed8b24bcb9ec85
IvFrol <130905461+IvFrol@users.noreply.github.com>
2023-05-24 16:10:48 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+new fullpage('#fullpage', {
+  //options here
+  autoScrolling:true,
+  scrollHorizontally: true,
+  sectionsColor : ['#fe5f46', '#0998ec', '#fc6c7c', '#fec304'],
+  navigation: true,
+  slidesNavigation: true,
+  anchors:['firstPage', 'secondPage', 'thirdPage', 'finalPage', 'SuperFinalPage'],
+  navigationTooltips: ['first', 'second']
+});
+
+// Koodi allikas: Rudnyi, M (2023) fullPage source code  [Source code]. https://github.com/MaksymRudnyi/fullPage
+
